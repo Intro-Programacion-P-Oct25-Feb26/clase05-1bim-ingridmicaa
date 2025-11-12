8d1bc30fdcdbcc693ac565729b898695ff2ff38f
ingridmicaa <ingridmicaela03@gmail.com>
2025-11-12 11:48:35 -0500

    Ingreso Teclado

--- ejemplos/Ejemplo1/src/ejemplo1/Ejemplo1.java
+++ ejemplos/Ejemplo1/src/ejemplo1/Ejemplo1.java
@@ -8,6 +8,8 @@
  */
 package ejemplo1;
 
+import java.util.Locale;
+import java.util.Scanner;
 /**
  *
  * @author reroes
@@ -16,8 +18,17 @@ public class Ejemplo1 {
 
     public static void main(String[] args) {
         // Condicionales simplesmain
-
-        double promedio = 7.5;
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        /* 
+        double promedio;
+        System.out.println("Ingrese un valor para analizar ");
+        double promedio= entrada.nextDouble();
+        */
+        
+        System.out.println("Ingrese un valor para analizar ");
+        double promedio= entrada.nextDouble();
+        
 
         if (promedio >= 7.5) {
             System.out.printf("Estudiante aprobado con un "
