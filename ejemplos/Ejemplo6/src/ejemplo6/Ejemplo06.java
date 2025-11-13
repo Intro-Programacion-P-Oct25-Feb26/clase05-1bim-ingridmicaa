41e7318f9576c434a49f8cbdd4c13b8c7e2f6689
ingridmicaa <ingridmicaela03@gmail.com>
2025-11-13 10:39:10 -0500

    Aprobado

--- ejemplos/Ejemplo6/src/ejemplo6/Ejemplo06.java
+++ ejemplos/Ejemplo6/src/ejemplo6/Ejemplo06.java
@@ -1,11 +1,11 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package ejemplo6;
 
-import java.io.PrintStream;
+
 import java.util.Locale;
 import java.util.Scanner;
 
